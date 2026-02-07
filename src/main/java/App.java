d7dbbce7d2928a74c2da5e449f9c18290b05a8c8
Gabrieldiog <gdiogo.software@gmail.com>
2026-02-06 23:47:57 -0300

    diminui as pastas e coloquei .class no gitignore

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,6 @@
-package com.gestao;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println("Financeira ");
+        System.out.println("Financeiro ");
 
     }
 }
