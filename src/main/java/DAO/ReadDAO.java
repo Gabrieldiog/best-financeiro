27729a9d3566b39084b303bc8966d1bb14a857f6
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-03-24 13:57:35 -0300

    Uma parte do "read" está feita

--- src/main/java/DAO/ReadDAO.java
+++ src/main/java/DAO/ReadDAO.java
@@ -1,5 +1,41 @@
 package DAO;
 
-public class ReadDAO {
+import model.Usuario;
+import utils.Conexao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadDAO {
+    public Usuario buscarUsuarioPorId(int id) {
+        String sql = "SELECT username FROM usuarios WHERE id = ?";
+        Usuario usuario = null;
+
+        try (Connection conn = Conexao.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                usuario = new Usuario();
+                usuario.setUsername(rs.getString("username"));
+                // Não adicionei senha nem ID aqui para focar só no nome que vai aparecer no extrato
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+    public List<String> listarMovimentacoes(int idUsuario){
+         String sql
+
+
+
+    }
 }
+
